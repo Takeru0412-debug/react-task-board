b2b35f79eebfd2c01ad781d51fe10aeef54aa84f
Takeru0412-debug <takerutsukareta0412@gmail.com>
2025-11-23 18:05:14 +0900

    fix: netlify base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/react-task-board/", // ★絶対必要
+  base: "/",   // ← これだけで直る！ ※絶対に / ！
   plugins: [react()],
 });
